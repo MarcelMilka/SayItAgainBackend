c3a44ffc53e412b09f34e80724f10612f8619049
Marcello <marcello.sw.dev@gmail.com>
2025-08-19 10:39:54 +0200

    feat(export/controller): implement CsvService, add the final logic

--- src/main/kotlin/eu/project/application/export/controller/ExportController.kt
+++ src/main/kotlin/eu/project/application/export/controller/ExportController.kt
@@ -1,18 +1,26 @@
 package eu.project.application.export.controller
 
 import eu.project.application.export.model.ExportDownloadRequest
+import eu.project.application.export.service.CsvService
 import jakarta.validation.Valid
+import org.springframework.http.HttpHeaders
+import org.springframework.http.MediaType
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-class ExportController {
+class ExportController(val csvService: CsvService) {
 
-    @PostMapping("/siaapi/exports/download")
-    fun exportDownload(@Valid @RequestBody exportDownloadRequest: ExportDownloadRequest): String {
+    @PostMapping("/siaapi/exports/download", produces = ["text/csv"])
+    fun exportDownload(@Valid @RequestBody exportDownloadRequest: ExportDownloadRequest): ResponseEntity<ByteArray> {
 
-        // TODO: replace with real logic
-        return exportDownloadRequest.wordsToExport.toString()
+        val csvBytes = csvService.prepareFileToExportWords(exportDownloadRequest.wordsToExport)
+
+        return ResponseEntity.ok()
+            .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"words.csv\"")
+            .contentType(MediaType.parseMediaType("text/csv"))
+            .body(csvBytes)
     }
 }
